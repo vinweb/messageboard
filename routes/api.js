4ee66a5597cf745f65cec20301c709cc2267e61e
Vincze János <vinczit@gmail.com>
2021-03-20 16:35:19 +0100

    added two GET and first DELETE requests

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 "use strict";
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const ObjectId = mongoose.Types.ObjectId;
 require("./db-connection");
 
 const replySchema = new Schema({
@@ -33,42 +34,120 @@ module.exports = function (app) {
             newThread.save((err, doc) => {
                 if (err) return console.log("Error: " + err);
                 console.log("Save is successful.");
-                return res.redirect("/b/board/");
+                return res.redirect(`/b/${board}/`);
             });
         })
         .get((req, res) => {
             let board = req.params.board;
             let thread = mongoose.model(board, threadSchema);
-            thread.find({}, (err, result) => {
-                console.log("find");
-                if (err) console.log(err);
-                res.json(result);
+            /* thread
+                .find({}, { replies: { $limit: 1 } })
+                .sort({ created_on: -1 })
+                .limit(10)
+                .exec((err, result) => {
+                    console.log("find");
+                    if (err) console.log(err);
+                    res.json(result);
+                }); */
+            thread
+                .aggregate([{ $limit: 10 }, { $sort: { created_on: -1 } }])
+                .exec((err, result) => {
+                    //console.log(result[9].replies);
+                    if (err) console.log(err);
+                    for (let thread of result) {
+                        delete thread.delete_password;
+                        delete thread.reported;
+                        if (thread.replies.length > 3) {
+                            thread.replies.splice(0, thread.replies.length - 3);
+                        }
+                        if (thread.replies.length > 0) {
+                            for (let reply of thread.replies) {
+                                delete reply.delete_password;
+                                delete reply.reported;
+                            }
+                            //console.log(thread.replies);
+                        }
+                    }
+                    res.json(result);
+                });
+        })
+        .delete((req, res) => {
+            // console.log(req.body);
+            // console.log(req.params);
+            let board = req.params.board;
+            let thread = mongoose.model(board, threadSchema);
+            let id = req.body.thread_id;
+            thread.findById(id, (err, doc) => {
+                if (err) {
+                    console.log(err);
+                    res.json("incorrect id");
+                } else if (req.body.delete_password === doc.delete_password) {
+                    doc.remove();
+                    res.json("success");
+                } else if (req.body.delete_password !== doc.delete_password) {
+                    res.json("incorrect password");
+                }
             });
-        });
-
-    app.route("/api/replies/:board").post(async (req, res) => {
-        let board = req.params.board;
-        let thread = mongoose.model(board, threadSchema);
-        let id = req.body.thread_id;
-
-        thread.findByIdAndUpdate(
-            id,
-            {
-                $set: { bumped_on: Date.now() },
-                $push: {
-                    replies: {
-                        text: req.body.text,
-                        delete_password: req.body.delete_password,
-                    },
-                },
-            },
-            function (err, docs) {
+            /* thread.findByIdAndRemove(id, (err, docs) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Updated thread.");
+                    alert("success");
                 }
-            }
-        );
-    });
+            }); */
+        });
+
+    app.route("/api/replies/:board")
+        .post(async (req, res) => {
+            let board = req.params.board;
+            let thread = mongoose.model(board, threadSchema);
+            let id = req.body.thread_id;
+
+            thread.findByIdAndUpdate(
+                id,
+                {
+                    $set: { bumped_on: Date.now() },
+                    $push: {
+                        replies: {
+                            text: req.body.text,
+                            delete_password: req.body.delete_password,
+                        },
+                    },
+                },
+                function (err, docs) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Updated thread.");
+                    }
+                }
+            );
+        })
+        .get((req, res) => {
+            let board = req.params.board;
+            let id = req.query.thread_id;
+            let thread = mongoose.model(board, threadSchema);
+            /* thread.findById(id, (err, doc) => {
+                if (err) console.log(err);
+                delete doc.delete_password;
+                console.log(doc.delete_password);
+                delete doc.reported;
+                console.log(doc);
+                res.json(doc);
+            }); */
+            thread
+                .aggregate([{ $match: { _id: ObjectId(id) } }])
+                .exec((err, doc) => {
+                    if (err) console.log(err);
+                    delete doc[0].reported;
+                    delete doc[0].delete_password;
+                    if (doc[0].replies.length > 0) {
+                        for (let reply of doc[0].replies) {
+                            delete reply.delete_password;
+                            delete reply.reported;
+                        }
+                    }
+                    res.json(doc);
+                });
+        });
 };
